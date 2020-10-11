ad7abdd5f33c02f31825e71fe00cfd83b5d415bf
rurik <urrast@yandex.ru>
2020-10-11 18:26:23 +0400

    ExpenceRepository

--- src/main/scala/ru/rurik/domain/expence/repository/ExpenceRepository.scala
+++ src/main/scala/ru/rurik/domain/expence/repository/ExpenceRepository.scala
@@ -1,12 +1,14 @@
 package ru.rurik.domain.expence.repository
 
 import ru.rurik.domain.expence.Expense
-import zio.Task
+import zio.{RIO, Task, ZIO}
 
 object ExpenceRepository {
 
   trait Service {
     def getById(id: Long): Task[Option[Expense]]
   }
+
+  def getById(id: Long): RIO[ExpenceRepository, Option[Expense]] = ZIO.accessM(_.get.getById(id))
 
 }
