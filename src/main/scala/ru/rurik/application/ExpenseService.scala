e9577a3da46e4100f80ee3f80b99263edd6760c3
rurik <urrast@yandex.ru>
2020-10-17 22:09:47 +0400

    constructExpenseTree

--- src/main/scala/ru/rurik/application/ExpenseService.scala
+++ src/main/scala/ru/rurik/application/ExpenseService.scala
@@ -1,59 +1,35 @@
 package ru.rurik.application
 
 import cats.implicits.catsSyntaxOptionId
-import ru.rurik.application.ExpenseService.fetchExpenseTree
 import ru.rurik.domain.expence.Expense
 import ru.rurik.domain.expence.repository.ExpenceRepository
-import ru.rurik.domain.expence.tree.{ExpenseTree, Tree}
-import zio.{RIO, Task, UIO, ZIO}
+import ru.rurik.domain.expence.tree.ExpenseTree
+import zio.{RIO, ZIO}
 
 object ExpenseService {
 
-  def constructExpenseTree(id: Long): RIO[ExpenceRepository, Option[ExpenseTree]] = {
-
-    for {
-      root <- ZIO.accessM[ExpenceRepository](_.get.getById(id))
-
-
-    } yield root
-
+  def constructExpenseTree(id: Long): RIO[ExpenceRepository, Option[ExpenseTree]] = ZIO.accessM[ExpenceRepository](_.get.getById(id)).flatMap {
+    case Some(expense) => fetchExpenseTree(expense).map(_.some)
+    case None => RIO.succeed[Option[ExpenseTree]](None)
   }
 
-  def fetchExpenseTree(expense: Expense): RIO[ExpenceRepository, Option[ExpenseTree]] = {
+  def fetchExpenseTree(expense: Expense): RIO[ExpenceRepository, ExpenseTree] = expense.subExpenses match {
+    case Some(ids) =>
+      for {
+        leafsOpt: Option[List[Expense]] <- ZIO.accessM[ExpenceRepository](_.get.getByIds(ids))
+        expenseTree: ExpenseTree <- fetchExpenseTreeWithSubExpenses(expense, leafsOpt.getOrElse(List.empty[Expense]))
+      } yield expenseTree
 
-    expense.subExpenses match {
-      case Some(ids) =>
-        val leafsRIO: RIO[ExpenceRepository, Option[List[Expense]]] = ZIO.accessM[ExpenceRepository](_.get.getByIds(ids))
-
-        leafsRIO.flatMap {
-          leafsOpt: Option[List[Expense]] =>
-            val option: Option[RIO[ExpenceRepository, ExpenseTree]] = leafsOpt.map {
-              leafs: List[Expense] =>
-
-                val value: RIO[ExpenceRepository, ExpenseTree] = ZIO.foldLeft(leafs)(List.empty[Option[ExpenseTree]]) {
-                  (list: List[Option[ExpenseTree]], leaf: Expense) =>
-                    fetchExpenseTree(leaf).map {
-                      expTree: Option[ExpenseTree] => list ++ List(expTree)
-                    }
-                }.map {
-                  expenseTrees: List[Option[ExpenseTree]] =>
-                    ExpenseTree(expense, expenseTrees.flatten.some)
-                }
-                value
+    case None => RIO(ExpenseTree(expense))
+  }
 
 
-            }
-            option
-
-
-        }
-
-
-      case None => RIO(Some(ExpenseTree(expense)))
+  def fetchExpenseTreeWithSubExpenses(expense: Expense, leafs: List[Expense]): RIO[ExpenceRepository, ExpenseTree] = {
+    ZIO.foldLeft(leafs)(List.empty[ExpenseTree]) {
+      (list, leaf) => fetchExpenseTree(leaf).map(list ++ List(_))
+    }.map {
+      expenseTrees => ExpenseTree(expense, expenseTrees.some)
     }
-
-
   }
-
 
 }
