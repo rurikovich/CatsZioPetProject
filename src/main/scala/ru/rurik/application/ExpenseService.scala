c225f50f8c061f36944b9069c9a5326fc5839a07
rurik <urrast@yandex.ru>
2020-11-07 02:03:34 +0400

    constructExpenseTree

--- src/main/scala/ru/rurik/application/ExpenseService.scala
+++ src/main/scala/ru/rurik/application/ExpenseService.scala
@@ -2,10 +2,27 @@ package ru.rurik.application
 
 import ru.rurik.domain.expence.Expense
 import ru.rurik.domain.expence.repository.ExpenceRepository
+import ru.rurik.domain.expence.tree.ExpenseTree
 import zio.{RIO, ZIO}
 
 object ExpenseService {
 
-  def testGetById(): RIO[ExpenceRepository, Option[Expense]] = ZIO.accessM[ExpenceRepository](_.get.getById(1))
+  def constructExpenseTree(id: Long): RIO[ExpenceRepository, Option[ExpenseTree]] = for {
+    expenseOpt: Option[Expense] <- ZIO.accessM[ExpenceRepository](_.get.getById(id))
+    subExpenses: List[Expense] <- ZIO.accessM[ExpenceRepository](_.get.getByParentId(id))
+    subExpensesRIOList: List[RIO[ExpenceRepository, Option[ExpenseTree]]] = subExpenses.map(_.id).map(constructExpenseTree)
+    expenseTreeList: List[ExpenseTree] <- toRIOList(subExpensesRIOList)
+  } yield expenseOpt.map(ExpenseTree(_, Some(expenseTreeList)))
+
+
+  def toRIOList[R, A](rioList: List[RIO[R, Option[A]]]): RIO[R, List[A]] = {
+    val startAcc: RIO[R, List[A]] = RIO.succeed[List[A]](List.empty)
+    rioList.foldLeft(startAcc) {
+      (rioList: RIO[R, List[A]], rio: RIO[R, Option[A]]) => {
+        rioList.zipWith(rio)((list, maybeTree) => list ::: maybeTree.map(List(_)).getOrElse(List.empty))
+      }
+    }
+  }
+
 
 }
--- src/test/scala/ru/rurik/application/ExpenseServiceSpec.scala
+++ src/test/scala/ru/rurik/application/ExpenseServiceSpec.scala
@@ -2,6 +2,7 @@ package ru.rurik.application
 
 import ru.rurik.domain.expence.Expense
 import ru.rurik.domain.expence.repository.ExpenceRepository
+import ru.rurik.domain.expence.tree.ExpenseTree
 import zio.test.Assertion.equalTo
 import zio.test._
 import zio.{Task, ZLayer}
@@ -12,7 +13,17 @@ object ExpenseServiceSpec extends DefaultRunnableSpec {
 
       testM("constructExpenseTree") {
 
-        assertM(ExpenseService.testGetById())(equalTo(Some(Expense(1, "name", 1, None))))
+        assertM(ExpenseService.constructExpenseTree(1))(
+          equalTo(
+            Some(
+              ExpenseTree(Expense(1, "name", 1, None),
+                Some(List(
+                  ExpenseTree(Expense(2, "name", 1, Some(1)), Some(List())),
+                  ExpenseTree(Expense(3, "name", 1, Some(1)), Some(List())))
+                ))
+            )
+          )
+        )
 
       }.provideSomeLayer(TestExpenceRepository.layer)
 
@@ -23,9 +34,19 @@ object ExpenseServiceSpec extends DefaultRunnableSpec {
 
 
 class TestExpenceRepository extends ExpenceRepository.Service {
-  override def getById(id: Long): Task[Option[Expense]] = Task(Some(Expense(1, "name", 1, None)))
 
-  override def getByParentId(id: Long): Task[List[Expense]] = Task(List())
+  val expenses = List(
+    Expense(1, "name", 1, None),
+    Expense(2, "name", 1, Some(1)),
+    Expense(3, "name", 1, Some(1))
+  )
+
+  //TODO replace by  expression using CATS
+  override def getById(id: Long): Task[Option[Expense]] = Task(expenses.find(_.id == id))
+
+  override def getByParentId(id: Long): Task[List[Expense]] = Task(
+    expenses.filter(_.parentId.contains(id))
+  )
 }
 
 object TestExpenceRepository {
